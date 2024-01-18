7b622bc990d0b3da5422fa347ba4b5187d1e1378
Jo <42.jochang@gmail.com>
2024-01-18 03:25:05 -0800

    fixed launcher bug

--- launcher.sh
+++ launcher.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 ##### variables #####
-python_cmd="python3"
+python_cmd="python3.10"
 required_version="3.10.0"
 venv_dir=".venv"
 use_venv=1
 config_file="config.json"
 
 ##### python #####
-if command -v "$python_cmd" &>/dev/null; then
+if command -v "$python_cmd" &>/dev/null
+then
 	python_version=$("$python_cmd" --version 2>&1 | awk '{print $2}')
-	if [ "$(printf '%s\n' "$required_version" "$python_version" | sort -V | head -n1)" != "$required_version" ]; then
+	if [ "$(printf '%s\n' "$required_version" "$python_version" | sort -V | head -n1)" != "$required_version" ]
+	then
 		printf "Error: Python $required_version or later is required. Current version is $python_version.\n"
 		exit 1
 	else
@@ -22,7 +24,7 @@ else
 fi
 
 ##### venv #####
-if [[ $use_venv -eq 1 ]] && [[ -z "${VIRTUAL_ENV}" ]];
+if [[ $use_venv -eq 1 ]] && [[ -z "${VIRTUAL_ENV}" ]]
 then
 	if ! "${python_cmd}" -c "import venv" &>/dev/null
 	then
@@ -48,19 +50,13 @@ else
 fi
 
 ##### requirements #####
-if ! pip list --format=columns | grep -qF -f requirements.txt
-then
-	printf "Requirements are already installed. Skipping installation.\n"
-else
-	pip install -r requirements.txt
-	printf "Requirements installed successfully.\n"
-fi
+pip install -r requirements.txt
 
 ##### config #####
 if [ ! -f "$config_file" ]
 then
 	printf "Creating config.json from template.\n"
-	cp "config_template.json" "config.json"
+	cp "config-template.json" "config.json"
 else
 	printf "${config_file} set as config.\n"
 fi
